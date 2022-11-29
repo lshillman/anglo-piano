e658c74771173bf85fa1fc818e590ffa71f6c6cc
Luke <lshillman@gmail.com>
2022-11-28 21:47:39 -0800

    add event listeners for input change and blur

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,5 +1,6 @@
+const viewerSection = document.getElementById("viewer");
 const editorContainer = document.getElementById("editor-container");
-const editorSection = document.getElementById("editor");
+const editor = document.getElementById("editor");
 const editLayoutBtn = document.getElementById("editLayoutBtn");
 
 
@@ -8,7 +9,9 @@ const editLayoutBtn = document.getElementById("editLayoutBtn");
 
 
 function renderEditor() {
+    currentMode = "edit"
     editorContainer.style.display = "block";
+    viewerSection.style.display = "none";
     editor.innerHTML = "";
     for (button of buttons) {
         let pushLabel = button.push;
@@ -16,9 +19,9 @@ function renderEditor() {
         if (button.newRow) {
             editor.innerHTML += `<br>`;
         }
-        editorSection.innerHTML += `<div class="editor-button" style="margin-left:${button.x}px"><div class="top"><input type=text value="${pushLabel}"></div><div class="bottom"><input type=text value="${pullLabel}"></div></div>`;
+        editor.innerHTML += `<div class="editor-button" style="margin-left:${button.x}px"><div class="top"><input type=text value="${pushLabel}"></div><div class="bottom"><input type=text value="${pullLabel}"></div></div>`;
     }
-
+    bindInputs();
 
 }
 
@@ -27,9 +30,16 @@ function renderEditor() {
 
 
 
-
-
-
+function bindInputs() {
+    let allfields = document.querySelectorAll(".editor-button input");
+    allfields.forEach((field) => field.addEventListener('input', (e) => {
+        console.log(e.target.value);
+    }
+    ));
+    allfields.forEach((field) => field.addEventListener('blur', (e) => {
+        console.log("Now validating " + e.target.value);
+    }));
+}
 
 
 
