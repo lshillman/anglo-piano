ee6d40d93972b7da0550c29450ce31ec6b6c4409
Luke <lshillman@gmail.com>
2022-12-03 21:48:33 -0800

    add function for inserting new buttons and rows

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -12,6 +12,11 @@ const layoutDownBtn = document.getElementById("layoutDown")
 
 const moveRightBtn = document.getElementById("moveRightBtn");
 const moveLeftBtn = document.getElementById("moveLeftBtn");
+const insertLeftBtn = document.getElementById("insertLeftBtn");
+const insertRightBtn = document.getElementById("insertRightBtn");
+const newRowBtn = document.getElementById("newRowBtn");
+
+
 const deleteBtn = document.getElementById("deleteBtn");
 const cancelBtn = document.getElementById("cancelBtn");
 const finishedBtn = document.getElementById("finishedBtn");
@@ -40,7 +45,7 @@ function renderEditor() {
         }
         editorKeyboard.innerHTML += `<div class="editor-button" style="margin-left:${button.x}px"><div class="top"><input type=text maxlength="3" placeholder="push" value="${pushLabel}"></div><div class="bottom"><input type=text maxlength="3"  placeholder="pull" value="${pullLabel}"></div></div>`;
     }
-    bindInputs();
+    bindInputs("all");
     currentField = document.querySelectorAll("#editor-anglo-keyboard input")[0];
     currentButton = document.querySelectorAll("#editor-anglo-keyboard .editor-button")[0];
     currentButton.classList.add("selected");
@@ -183,8 +188,13 @@ function transposeLayout(direction) {
 
 
 
-function bindInputs() {
-    let allfields = document.querySelectorAll(".editor-button input");
+function bindInputs(fields) {
+    let allfields;
+    if (fields == "all") {
+        allfields = document.querySelectorAll(".editor-button input");
+    } else if (fields = "new") {
+        allfields = document.querySelectorAll(".newbutton input");
+    }
     allfields.forEach((field) => field.addEventListener('input', (e) => {
         if (isValid(e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1).toLowerCase())) {
             e.target.classList.remove("invalid");
@@ -203,10 +213,6 @@ function bindInputs() {
         if (!isValid(e.target.value)) {
             e.target.classList.add("invalid");
         }
-        // this removes a button with no push or pull values. Probably not needed now that we have an explicit 'delete' button.
-        // if (!e.target.parentNode.parentNode.firstChild.firstChild.value && !e.target.parentNode.parentNode.lastChild.firstChild.value && !e.target.parentNode.parentNode.contains(e.relatedTarget)) {
-        //     deleteButton(e.target.parentNode.parentNode);
-        // }
     }));
     allfields.forEach((field) => field.addEventListener('keydown', (e) => {
         if (e.code.indexOf('Shift') != -1) {
@@ -215,7 +221,6 @@ function bindInputs() {
                 e.target.value = editorNotes[e.target.value].next;
             }
             playNote(noteNames[e.target.value]);
-            // console.log(`Target value: ${e.target.value}. This value: ${this.value}.`);
         } else if (e.code == "ArrowDown") {
             if (editorNotes[e.target.value].prev) {
                 e.target.value = editorNotes[e.target.value].prev;
@@ -231,6 +236,11 @@ function bindInputs() {
             deleteButton(currentButton);
         }
     }));
+    if (fields == "new") {
+        document.querySelectorAll(".newbutton").forEach((button) => {
+            button.classList.remove("newbutton");
+        })
+    }
 }
 
 
@@ -248,6 +258,19 @@ function moveButton(direction) {
     } else if (direction == "right") {
         currentButton.style.marginLeft = `${margin + 10}px`;
     }
+}
+
+function insertButton(where) {
+    let position;
+    let newButton = `<div class="editor-button newbutton" style="margin-left:0px"><div class="top"><input type=text maxlength="3" placeholder="push" value=""></div><div class="bottom"><input type=text maxlength="3" placeholder="pull" value=""></div></div>`;
+    if (where == "left") {
+        currentButton.insertAdjacentHTML("beforebegin", newButton);
+    } else if (where == "right") {
+        currentButton.insertAdjacentHTML("afterend", newButton);
+    } else if (where == "newRow") {
+        editorKeyboard.lastChild.insertAdjacentHTML("afterend", "<br>" + newButton);
+    }
+    bindInputs("new");
 }
 
 
@@ -274,6 +297,18 @@ moveRightBtn.addEventListener("click", () => {
 
 moveLeftBtn.addEventListener("click", () => {
     moveButton("left");
+});
+
+insertLeftBtn.addEventListener("click", () => {
+    insertButton("left");
+});
+
+insertRightBtn.addEventListener("click", () => {
+    insertButton("right");
+});
+
+newRowBtn.addEventListener("click", () => {
+    insertButton("newRow");
 });
 
 deleteBtn.addEventListener("click", () => {
