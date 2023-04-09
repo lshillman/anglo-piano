704b31b37ed2ac280160bea19f136839bfef3987
Luke <lshillman@gmail.com>
2023-04-08 17:29:57 -0700

    add ui for creating, deleting, and selecting sequences

--- assets/js/sequencer.js
+++ assets/js/sequencer.js
@@ -3,13 +3,14 @@ let selectionMode = "notes"
 
 // sequencer elements
 const seq_dropdown = document.getElementById("sequence");
-const seq_new = document.getElementById("seq-new");
-const seq_createBtn = document.getElementById("createSequenceBtn");
-const seq_save = document.getElementById("seq-save");
-const seq_update = document.getElementById("seq-update");
+const seq_new = document.getElementById("seq-new"); // shows the create modal
+const seq_createBtn = document.getElementById("createSequenceBtn"); // inside the create modal
 const seq_delete = document.getElementById("seq-delete");
-const seq_next = document.getElementById("seq-next");
-const seq_prev = document.getElementById("seq-prev");
+const frame_save = document.getElementById("frame-save");
+const frame_update = document.getElementById("frame-update");
+const frame_delete = document.getElementById("frame-delete");
+const frame_next = document.getElementById("frame-next");
+const frame_prev = document.getElementById("frame-prev");
 const timeline = document.getElementById("timeline");
 
 
@@ -50,6 +51,7 @@ function createSequence() {
             frames: []
         };
         writeSequences();
+        loadSequences();
     }
 }
 
@@ -58,6 +60,7 @@ let currentSelection = -1;
 function saveSelection(position = sequences[seq_dropdown.value].frames.length) {
     let frames = sequences[seq_dropdown.value].frames;
     frames.push({bellows: opt_bellows, mode: selectionMode, notes: [...selection], buttons: [...buttonSelection]});
+    writeSequences();
     timeline.innerHTML += `<div class="sequencer-frame" data-position="${frames.length - 1}">${frames.length}</div>`
     currentSelection = position;
     updateSelectedFrames();
@@ -115,13 +118,30 @@ function loadPrevSelection() {
 }
 
 function populateTimeline() {
-    let frames = sequences[seq_dropdown.value].frames;
-    timeline.innerHTML = "";
-    if (frames && frames.length != 0) {
-        for (let i = 0; i < frames.length; i++) {
-            timeline.innerHTML += `<div class="sequencer-frame" data-position="${i}">${i + 1}</div>`
+    if (seq_dropdown.value) {
+        let frames = sequences[seq_dropdown.value].frames;
+        timeline.innerHTML = "";
+        if (frames && frames.length != 0) {
+            for (let i = 0; i < frames.length; i++) {
+                timeline.innerHTML += `<div class="sequencer-frame" data-position="${i}">${i + 1}</div>`
+            }
         }
+    } else {
+        timeline.innerHTML = "";
     }
+}
+
+function confirmDelete() {
+    document.getElementById("confirmDeleteSequenceMsg").innerText = `Do you really want to delete "${seq_dropdown.value}"?`
+    document.getElementById("delete-sequence-modal").style.display = "block";
+}
+
+function deleteSequence() {
+    delete sequences[seq_dropdown.value];
+    seq_dropdown.remove(seq_dropdown.selectedIndex);
+    writeSequences();
+    closeModal();
+    populateTimeline();
 }
 
 
@@ -130,11 +150,12 @@ seq_dropdown.addEventListener("change", () => {
 });
 seq_createBtn.addEventListener("click", () => createSequence());
 seq_new.addEventListener("click", () => promptForTitle());
-seq_save.addEventListener("click", () => saveSelection());
-seq_update.addEventListener("click", () => updateSelection());
-seq_delete.addEventListener("click", () => deleteSelection());
-seq_next.addEventListener("click", () => loadNextSelection());
-seq_prev.addEventListener("click", () => loadPrevSelection());
+seq_delete.addEventListener("click", () => confirmDelete());
+frame_save.addEventListener("click", () => saveSelection());
+frame_update.addEventListener("click", () => updateSelection());
+frame_delete.addEventListener("click", () => deleteSelection());
+frame_next.addEventListener("click", () => loadNextSelection());
+frame_prev.addEventListener("click", () => loadPrevSelection());
 timeline.addEventListener("click", (e) => {
     if(e.target && e.target.className.includes("sequencer-frame")) {
         currentSelection = parseInt(e.target.dataset.position);
